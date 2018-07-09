18a266a1333386726bc01186f7f82d6612a49b52
Susanna Kosonen <susannakosonen@Susannas-MacBook-Pro.local>
2018-07-09 17:52:51 -0400

    Added column url to table jobs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_09_194428) do
+ActiveRecord::Schema.define(version: 2018_07_09_215139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2018_07_09_194428) do
     t.datetime "updated_at", null: false
     t.string "company"
     t.string "city"
+    t.string "url"
   end
 
   create_table "tasks", force: :cascade do |t|
