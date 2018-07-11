de23d7eaad23a8874b7b75f941d267d91d6ba208
jchamberlain909 <jchamberlain909@gmail.com>
2018-07-11 15:56:35 -0400

    Fixed edits hopefully

--- app/assets/javascripts/task.js
+++ app/assets/javascripts/task.js
@@ -2,8 +2,9 @@
 
 function showEditForm(){
     event.stopPropagation()
+    debugger
     var item_text = event.target.firstChild.textContent
-    var item_path = $(event.target).children().attr('href')
+    var item_path = $(event.target).parent().find('.link-to').children().attr('href')
     var form_authenticity_token = $('#authenticity')[0].textContent
     var form_string = `<form class="form-inline" action="${item_path}" method="post" accept-charset='UTF-8'><input name="utf8" type="hidden" value="✓"><input type="hidden" name="authenticity_token" value="${form_authenticity_token}"><input type="hidden" name="_method" value="patch"> <input type="text" name="task[description]" value="${item_text}"> <button type="submit" class="btn btn-primary">Update</button> </form>`
     $(event.target).replaceWith(form_string)
