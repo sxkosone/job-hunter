3362744665c1cf1c8328acd2d5fb5a5ac01115ce
Susanna Kosonen <susannakosonen@Susannas-MacBook-Pro.local>
2018-07-12 17:28:54 -0400

    added specs for user sessions and log ins

---
+++ spec/controllers/user_sessions_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe JobsController do
+    before do
+        password = "pass"
+        @user = User.create(name: "Ricky Rspek", username: "ricky1234", password: password)
+    end
+
+    describe 'get index' do
+        it "should redirect to login if you're not logged in" do
+        get :index
+        expect(response).to redirect_to controller: 'static', action: 'home'
+        end
+    end
+
+    describe 'logged_in_user' do
+        it 'returns nil if nobody is logged in' do
+            expect(subject.logged_in_user).to be nil
+        end
+
+        it "returns username for logged in users" do
+            user = User.first
+            @request.session[:user_id] = user.id
+            expect(subject.logged_in_user).to eq user.username
+        end
+    end
+end
