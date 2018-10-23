392812435a5b50b81ad1c736b968ad6b5341bfef
Susanna Kosonen <susannakosonen@Susannas-MacBook-Pro.local>
2018-10-23 17:00:44 -0400

    gem faker causing issues on dev environment, commented the it out from gemfile because its unused anyway

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
 require 'faker'
+I18n.reload! #trying to fix faker gem dependency, which causes errors
+
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
